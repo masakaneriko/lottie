1694cc916a707a92fc8021663be9f827fccff706
lc-masa <myamanakalife6911@gmail.com>
2023-05-12 16:56:14 +0900

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('lottie-container'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'cute_trashcan.json'
+});
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById('particlosion'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'particle_explosion.json'
+});
